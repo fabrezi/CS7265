e057c72898fcd65b9b0b9637580f215fa82b855c
Farid Khan <fkschon@gmail.com>
2019-02-15 12:51:48 -0500

    Create dtree.py

---
+++ Decision Tree/dtree.py
@@ -0,0 +1,7 @@
+#Decision Tree classifier.
+#upload the data and display output
+import pandas as pd
+
+data = pd.read_excel('C:\\Users\\farid-PC\\Desktop\\class\\CS7265_BIG_DATA\\demo_hw2\\tennis.xlsx')
+pd.set_option('display.max_colwidth', 1000)
+print(data)
