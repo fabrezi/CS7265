cb6b5dcb0ff500d60fa809d7241f25e991e08154
fabrezi <fkschon@gmail.com>
2019-03-07 15:56:26 -0500

    Update hw3.py

--- linear regression/hw3.py
+++ linear regression/hw3.py
@@ -4,7 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+#######################################################################
+#Classifier {task, experience, performance]
+#linear regression: is a supervised learning model in which the prediction is made on the expected relation between two variables X (independent variable) and Y (dependent variable)
+#formula: Y = mX + c + e
+#specific formula: y' = w^T + c
 
+############################################################################
 #upload data
 data = pd.read_csv("C:\\Users\\farid-PC\\Desktop\\class\\CS7265_BIG_DATA\\HW3\\housing_training.csv", header=None)
 # CRIM: crime rate by town per capita || ZN: land zoned for lots(sq.ft) || INDUS: non-retail land(sq.ft) || CHAS: 1 - bound river 0 - null || NOX: nitro oxide concentration(per 10 million)
@@ -12,8 +18,8 @@ data = pd.read_csv("C:\\Users\\farid-PC\\Desktop\\class\\CS7265_BIG_DATA\\HW3\\h
 # LSTAT: percent || MEDV (target variable): median value of the owner occupied homes in 1000
 # Predict: the cost of the house
 data.rename(columns= {0: 'CRIM', 1: 'ZN', 2: 'INDUS', 3: 'CHAS', 4: 'NOX', 5: 'RM', 6: 'AGE', 7: 'DIS', 8: 'RAD', 9: 'TAX', 10: 'PTRATIO', 11: 'B', 12: 'LSTAT', 13: 'MEDV'}, inplace=True)
-#print(data)
-print(data.head())
+print(data)
+#print(data.head())
 
 print('\n')
 
@@ -21,26 +27,14 @@ print('\n')
 #correlation matrix
 corr_matrix = data.corr().round(2)
 sns.heatmap(data=corr_matrix, annot=True, fmt="g", cmap='viridis')
-plt.show()
+#plt.show()
 
-
-
-#test_method
-#def plotFeatures(col_list):
-  #  plt.figure(figsize=(10,14))
-   # i=0
-    #print(len(col_list))
-    #for col in col_list:
-     #   i += 1
-      #  plt.subplot(7,2,i)
-       # plt.plot(data[col], data["MEDV"], marker='.', linestyle='none')
-        #plt.title(title%(col))
-        #plt.tight_layout()
-#colnames = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NPX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT']
-#plotFeatures(colnames)
+#display scatterplot of each independent variable??
 
 #measure the sample correlation cofficient
 mm = data["MEDV"].mean()
 aa = data["CRIM"].mean()
 print("MEDV mean:" , mm)
 print(aa)
+
+
